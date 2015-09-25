fde1f8023a991f85b530bda90a9b6bb5778c92ae
BalmungSan <luis.miguel.mejia@hotmail.com>
2015-09-24 23:41:52 -0500

    ALL READY

--- OrganizacionPractica2/OrganizacionPractica2/OrganizacionPractica2.cpp
+++ OrganizacionPractica2/OrganizacionPractica2/OrganizacionPractica2.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+using namespace std;
 
 //CONSTANT VARIABLES
 const double grades_to_radians_constant = 0.0174532925;
@@ -76,14 +77,85 @@ void fdiv() {
 	cout << "****** RESULTADO: " << first_operand << endl;
 }
 
-double fsqrt_auxiliary(double operand) {
-	double result = 0;
+double abs_auxiliary(double x) {
+	double zero = 0, negate = -1, result;
 
-	//squirt
 	__asm {
+		//load x in xmm0
+		movsd xmm0, [x]
+
+			//check if x is >= 0
+			comisd xmm0, [zero]
+			jae fin
+
+			//if not, multiply by -1
+			mulsd xmm0, [negate]
+
+			fin:
+		//load xmm0 in result
+		movsd[result], xmm0
 	}
 
 	return result;
+}
+
+void abs() {
+	//Variables declaration
+	double operand, result;
+
+	//Variables initialization
+	cout << "****** INGRESA OPERANDO: ";
+	cin >> operand;
+
+	//ASM code
+	__asm {
+		sub esp, 8
+			movsd xmm0, [operand]
+			movsd[esp], xmm0
+			call abs_auxiliary
+			add esp, 8
+			fstp[result]
+	}
+	cout << "****** RESULTADO: " << result << endl;
+}
+
+double fsqrt_auxiliary(double x) {
+	double var, a, g = 1, two = 2, precision = 0.001;
+
+	//squirt
+	__asm {
+	sqr:
+		//a = x/g
+		movsd xmm0, [x]
+			divsd xmm0, [g]
+			movsd[a], xmm0
+
+			//g = (g + a)/2
+			movsd xmm0, [g]
+			addsd xmm0, [a]
+			divsd xmm0, [two]
+			movsd[g], xmm0
+
+			//var = (g - a)
+			movsd xmm0, [g]
+			subsd xmm0, [a]
+			movsd[var], xmm0
+
+			//var = abs(g - a)
+			sub esp, 8
+			movsd xmm0, [var]
+			movsd[esp], xmm0
+			call abs_auxiliary
+			add esp, 8
+			fstp[var]
+
+			//repeat while var > precision
+			movsd xmm0, [var]
+			comisd xmm0, [precision]
+			jae sqr
+	}
+
+	return g;
 }
 
 void fsqrt() {
@@ -102,16 +174,22 @@ void fsqrt() {
 	}
 	cout << "****** RESULTADO (coprocesador): " << proc << endl;
 
-	//ASM code (our implementation)
-	__asm {
-		sub esp, 8
-			movsd xmm0, [operand]
-			movsd[esp], xmm0
-			call fsqrt_auxiliary
-			add esp, 8
-			fstp[us]
+	cout << "****** RESULTADO (Series): ";
+	if (operand >= 0) {
+		//ASM code (our implementation)
+		__asm {
+			sub esp, 8
+				movsd xmm0, [operand]
+				movsd[esp], xmm0
+				call fsqrt_auxiliary
+				add esp, 8
+				fstp[us]
+		}
+		cout << us << endl;
 	}
-	cout << "****** RESULTADO (Series): " << us << endl;
+	else {
+		cout << "MATH ERROR" << endl;
+	}
 }
 
 double fpot_auxiliary(double base, int exponent) {
@@ -131,8 +209,8 @@ double fpot_auxiliary(double base, int exponent) {
 		loop mult
 			jmp fin
 
-		mult :
-			movsd xmm0, [result]
+			mult :
+		movsd xmm0, [result]
 			mulsd xmm0, [base]
 			movsd[result], xmm0
 			jmp  ciclo
@@ -162,22 +240,22 @@ int ffact_auxiliary(int base) {
 	__asm {
 		// ECX = base
 		mov ecx, [base]
-		
-		cmp [base], 0
-		jne mult
-		
-		case_0 :
-			mov eax, [one]
+
+			cmp[base], 0
+			jne mult
+
+			case_0 :
+		mov eax, [one]
 			mov[result], eax
 			jmp endd
 
-		mult:
-			mov eax, [result]
+			mult :
+		mov eax, [result]
 			mul ecx
 			mov[result], eax
 			loop mult
 
-		endd:
+			endd :
 
 	}
 
@@ -207,48 +285,48 @@ double fsin_auxiliary(double radians) {
 	__asm {
 	taylor:
 		//(-1)^n
-		push[iteration] //Push final argument (n = tolerance)
-		sub esp, 8
-		movsd xmm0, [sign]
-		movsd[esp], xmm0 //Push first argument (-1)
-		call fpot_auxiliary //Call pot
-		add esp, 0Ch
-		fstp[num] //num = -1^n
+		push[iteration] //Push final argument (n = iteration)
+			sub esp, 8
+			movsd xmm0, [sign]
+			movsd[esp], xmm0 //Push first argument (-1)
+			call fpot_auxiliary //Call pot
+			add esp, 0Ch
+			fstp[num] //num = -1^n
 
-		//2n + 1
-		mov eax, [iteration] //Move tolerance (n) to eax
-		mul[two] //eax = eax * 2 -> eax = 2n
-		inc eax //eax++
-		mov[var], eax //var = 2n + 1
+			//2n + 1
+			mov eax, [iteration] //Move tolerance (n) to eax
+			mul[two] //eax = eax * 2 -> eax = 2n
+			inc eax //eax++
+			mov[var], eax //var = 2n + 1
 
-		//(2n + 1)!
-		push[var] //push var
-		call ffact_auxiliary //call factorial
-		add esp, 4
-		mov[den], eax //den = (2n + 1)!
-		cvtsi2sd xmm0, [den]
-		movsd[dden], xmm0
+			//(2n + 1)!
+			push[var] //push var
+			call ffact_auxiliary //call factorial
+			add esp, 4
+			mov[den], eax //den = (2n + 1)!
+			cvtsi2sd xmm0, [den]
+			movsd[dden], xmm0
 
-		//x^(2n+1)
-		push[var] //Push final argument (2n+1)
-		sub esp, 8
-		movsd xmm0, [radians]
-		movsd[esp], xmm0 //Push first argument (x = radians)
-		call fpot_auxiliary //Call pot
-		add esp, 0Ch
-		fstp[x] //x = x^(2n+1)
+			//x^(2n+1)
+			push[var] //Push final argument (2n+1)
+			sub esp, 8
+			movsd xmm0, [radians]
+			movsd[esp], xmm0 //Push first argument (x = radians)
+			call fpot_auxiliary //Call pot
+			add esp, 0Ch
+			fstp[x] //x = x^(2n+1)
 
-		//(-1^n /(2n+1)!) * x^(2n+1)
-		movsd xmm0, [num] //- 1 ^ n
-		divsd xmm0, [dden] //-1^n /(2n+1)!
-		mulsd xmm0, [x] //(-1^n /(2n+1)!) * x^(2n+1)
-		addsd xmm0, [result]
-		movsd[result], xmm0 //result += (-1^n /(2n+1)!) * x^(2n+1
+			//(-1^n /(2n+1)!) * x^(2n+1)
+			movsd xmm0, [num] //- 1 ^ n
+			divsd xmm0, [dden] //-1^n /(2n+1)!
+			mulsd xmm0, [x] //(-1^n /(2n+1)!) * x^(2n+1)
+			addsd xmm0, [result]
+			movsd[result], xmm0 //result += (-1^n /(2n+1)!) * x^(2n+1
 
-		//Repeat until iteration < limit 
-		inc[iteration]
-		cmp[iteration], 9 //Change this value for chage the number of repetitions
-		jne taylor
+			//Repeat until iteration < limit 
+			inc[iteration]
+			cmp[iteration], 10 //Change this value for chage the number of repetitions
+			jne taylor
 	}
 
 	return result;
@@ -264,18 +342,18 @@ void fsin() {
 	//ASM code (processor)
 	_asm {
 		fld[degrees] //Load the float variable
-		fsin //Sin function
-		fstp[proc] //Store the float variable
+			fsin //Sin function
+			fstp[proc] //Store the float variable
 	}
 	cout << "****** RESULTADO (coprocesador): " << proc << endl;
 	//ASM code (our implementation)
 	__asm {
 		sub esp, 8
-		movsd xmm0, [degrees]
-		movsd[esp], xmm0
-		call fsin_auxiliary
-		add esp, 8
-		fstp[us]
+			movsd xmm0, [degrees]
+			movsd[esp], xmm0
+			call fsin_auxiliary
+			add esp, 8
+			fstp[us]
 	}
 
 	cout << "****** RESULTADO (Series): " << us << endl;
@@ -296,47 +374,47 @@ double fcos_auxiliary(double radians) {
 	__asm {
 	taylor:
 		//(-1)^n
-		push[iteration] //Push final argument (n = tolerance)
-		sub esp, 8
-		movsd xmm0, [sign]
-		movsd[esp], xmm0 //Push first argument (-1)
-		call fpot_auxiliary //Call pot
-		add esp, 0Ch
-		fstp[num] //num = -1^n
+		push[iteration] //Push final argument (n = iteration)
+			sub esp, 8
+			movsd xmm0, [sign]
+			movsd[esp], xmm0 //Push first argument (-1)
+			call fpot_auxiliary //Call pot
+			add esp, 0Ch
+			fstp[num] //num = -1^n
 
-		//2n
-		mov eax, [iteration] //Move tolerance (n) to eax
-		mul[two] //eax = eax * 2 -> eax = 2n
-		mov[var], eax //var = 2n
+			//2n
+			mov eax, [iteration] //Move tolerance (n) to eax
+			mul[two] //eax = eax * 2 -> eax = 2n
+			mov[var], eax //var = 2n
 
-		//(2n)!
-		push[var] //push var
-		call ffact_auxiliary //call factorial
-		add esp, 4
-		mov[den], eax //den = (2n)!
-		cvtsi2sd xmm0, [den]
-		movsd[dden], xmm0
+			//(2n)!
+			push[var] //push var
+			call ffact_auxiliary //call factorial
+			add esp, 4
+			mov[den], eax //den = (2n)!
+			cvtsi2sd xmm0, [den]
+			movsd[dden], xmm0
 
-		//x^(2n)
-		push[var] //Push final argument (2n)
-		sub esp, 8
-		movsd xmm0, [radians]
-		movsd[esp], xmm0 //Push first argument (x = radians)
-		call fpot_auxiliary //Call pot
-		add esp, 0Ch
-		fstp[x] //x = x^(2n)
+			//x^(2n)
+			push[var] //Push final argument (2n)
+			sub esp, 8
+			movsd xmm0, [radians]
+			movsd[esp], xmm0 //Push first argument (x = radians)
+			call fpot_auxiliary //Call pot
+			add esp, 0Ch
+			fstp[x] //x = x^(2n)
 
-		//(-1^n /(2n)!) * x^(2n)
-		movsd xmm0, [num] //- 1 ^ n
-		divsd xmm0, [dden] //-1^n /(2n)!
-		mulsd xmm0, [x] //(-1^n /(2n+1)!) * x^(2n)
-		addsd xmm0, [result]
-		movsd[result], xmm0 //result += (-1^n /(2n)!) * x^(2n)
+			//(-1^n /(2n)!) * x^(2n)
+			movsd xmm0, [num] //- 1 ^ n
+			divsd xmm0, [dden] //-1^n /(2n)!
+			mulsd xmm0, [x] //(-1^n /(2n+1)!) * x^(2n)
+			addsd xmm0, [result]
+			movsd[result], xmm0 //result += (-1^n /(2n)!) * x^(2n)
 
-		//Repeat until iteration < limit 
-		inc[iteration]
-		cmp[iteration], 9 //Change this value for chage the number of repetitions
-		jne taylor
+			//Repeat until iteration < limit 
+			inc[iteration]
+			cmp[iteration], 10 //Change this value for chage the number of repetitions
+			jne taylor
 	}
 
 	return result;
@@ -344,27 +422,33 @@ double fcos_auxiliary(double radians) {
 
 void fcos() {
 	//Variables declaration
-	double degrees, proc, us;
+	double degrees, rad, proc, us;
 	//Variables initialization
 	cout << "****** INGRESA ANGULO EN GRADOS: ";
 	cin >> degrees;
-	degrees *= grades_to_radians_constant;
+	rad = degrees * grades_to_radians_constant;
 
 	//ASM code
 	_asm {
-		fld[degrees] //Load the float variable
-		fcos //Cos function
-		fstp[proc] //Store the float variable
+		fld[rad] //Load the float variable
+			fcos //Cos function
+			fstp[proc] //Store the float variable
 	}
 	cout << "****** RESULTADO (Funcion coprocesador): " << proc << endl;
-	//ASM code (our implementation)
-	__asm {
-		sub esp, 8
-		movsd xmm0, [degrees]
-		movsd[esp], xmm0
-		call fcos_auxiliary
-		add esp, 8
-		fstp[us]
+
+	if (degrees != 90) {
+		//ASM code (our implementation)
+		__asm {
+			sub esp, 8
+				movsd xmm0, [rad]
+				movsd[esp], xmm0
+				call fcos_auxiliary
+				add esp, 8
+				fstp[us]
+		}
+	}
+	else {
+		us = 0;
 	}
 
 	cout << "****** RESULTADO (Series): " << us << endl;
@@ -372,47 +456,51 @@ void fcos() {
 
 void ftan() {
 	//Variables declaration
-	double degrees, result;
+	double degrees, rad, result;
 	//Variables initialization
 	cout << "****** INGRESA ANGULO EN GRADOS: ";
 	cin >> degrees;
-	degrees *= grades_to_radians_constant;
+	rad = degrees * grades_to_radians_constant;
 	//ASM code
 	_asm {
-		fld[degrees] //Load the float variable
-		fsin //Sin function
-		fld[degrees] //
-		fcos
-		fdivp st(1), st(0)
-		fstp[result]
+		fld[rad] //Load the float variable
+			fsin //Sin function
+			fld[rad] //
+			fcos
+			fdivp st(1), st(0)
+			fstp[result]
 	}
 	cout << "****** RESULTADO (Funcion coprocesador): " << result << endl;
 
-	double sin, dsin, cos, dcos;
+	cout << "****** RESULTADO (Series): ";
+	if (degrees != 90) {
+		double sin, cos;
+		_asm {
+			//sin(rad)
+			sub esp, 8
+				movsd xmm0, [rad]
+				movsd[esp], xmm0
+				call fsin_auxiliary //Call sin
+				add esp, 8
+				fstp[sin]
 
-	_asm {
-		//sin(degrees)
-		sub esp, 8
-		movsd xmm0, [degrees]
-		movsd[esp], xmm0
-		call fsin_auxiliary //Call sin
-		add esp, 8
-		fstp[sin]
+				//cos(rad)
+				sub esp, 8
+				movsd xmm0, [rad]
+				movsd[esp], xmm0
+				call fcos_auxiliary //Call cos
+				add esp, 8
+				fstp[cos]
 
-		//cos(degrees)
-		sub esp, 8
-		movsd xmm0, [degrees]
-		movsd[esp], xmm0
-		call fcos_auxiliary //Call cos
-		add esp, 8
-		fstp[cos]
-		
-		//tan = sin(degrees)/cos(degrees)
-		movsd xmm0, [sin] //sin(degrees)
-		divsd xmm0, [cos] //sin(degrees)/cos(degrees)
-		movsd[result], xmm0 //result = sin(degrees)/cos(degrees)
+				//tan = sin(degrees)/cos(degrees)
+				movsd xmm0, [sin] //sin(degrees)
+				divsd xmm0, [cos] //sin(degrees)/cos(degrees)
+				movsd[result], xmm0 //result = sin(degrees)/cos(degrees)
+		}
 	}
-	cout << "****** RESULTADO (Series): " << result << endl;
+	else {
+		cout << "MATH ERROR " << endl;
+	}
 }
 
 
@@ -420,48 +508,101 @@ void ftan() {
 //LOGARITHMIC OPERATIONS
 
 //2 * SUM( ((x-1)/(x+1))^(2*n-1) / (2*n -1)
-double fln(double x) {
-	double num_1, num_2, num; //Numerator
-	double den, one = 1, two = 2, result = 0;
-	int exp, iteration = 0, var;
+double fln_auxiliary(double x) {
+	double num_1, num_2, num_3, num; //Numerator
+	double den, one = 1, result = 0, var;
+	int exp, iteration = 0, two = 2;
 
-	_asm {
-		//num_1 = x-1
-		movsd xmm0, [x]
-		subsd xmm0, [one]
-		movsd [num_1],xmm0
+	if (x > 0) {
+		_asm {
+			//num_1 = x-1
+			movsd xmm0, [x]
+				subsd xmm0, [one]
+				movsd[num_1], xmm0
 
-		//num_2 = x+1
-		movsd xmm0, [x]
-		addsd xmm0, [one]
-		movsd [num_2],xmm0
-		
-		//num = num_1/num_2
-		movsd [num], [num_1]
-		movsd xmm0, [num_2]
-		divsd [num], xmm0
+				//num_2 = x+1
+				movsd xmm0, [x]
+				addsd xmm0, [one]
+				movsd[num_2], xmm0
 
-		//var = 2*n + 1
-		movsd xmm0, [iteration]
-		mulsd xmm0, 2
-		addsd xmm0, 1
-		movsd [var], xmm0
+				//num_3 = num_1/num_2
+				movsd xmm0, [num_1]
+				divsd xmm0, [num_2]
+				movsd[num_3], xmm0
 
-		//num^var
-		push[var]
+				summatory :
+
+			//exp = 2*n + 1
+			mov eax, [iteration]
+				mul[two]
+				mov[exp], eax
+				inc exp
+
+				//var = (double)exp
+				cvtsi2sd xmm0, [exp]
+				movsd[var], xmm0
+
+				//num = num_3^exp
+				push[exp]
+				sub esp, 8
+				movsd xmm0, [num_3]
+				movsd[esp], xmm0 //Push first argument (base)(x)
+				call fpot_auxiliary //Call pot
+				add esp, 0Ch
+				fstp[num]
+
+				//den = num^exp / var
+				movsd xmm0, [num]
+				divsd xmm0, [var]
+				movsd[den], xmm0
+
+				//result += den
+				movsd xmm0, [result]
+				addsd xmm0, [den]
+				movsd[result], xmm0
+
+				//Repeat until iteration < limit 
+				inc[iteration]
+				cmp[iteration], 100 //Change this value for chage the number of repetitions
+				jne summatory
+
+				//Result = Result*2
+				movsd xmm0, [result]
+				addsd xmm0, [result]
+				movsd[result], xmm0
+		}
+	}
+
+	return result;
+}
+
+void fln() {
+	//Variables declaration
+	double operand, result;
+
+	//Variables initialization
+	cout << "****** INGRESA OPERANDO: ";
+	cin >> operand;
+
+	//ASM code
+	__asm {
 		sub esp, 8
-		movsd xmm0, [num]
-		movsd[esp], xmm0 //Push first argument (base)(x)
-		call fpot_auxiliary //Call pot
-		add esp, 0Ch
-		fstp[num] 
+			movsd xmm0, [operand]
+			movsd[esp], xmm0
+			call fln_auxiliary
+			add esp, 8
+			fstp[result]
+	}
+	cout << "****** RESULTADO (Series): ";
 
-		//num^var / var
-		movsd xmm0, [num]
-		divsd xmm0, [var]
-		movsd [result], xmm0
+	if (result == 0) {
+		cout << "MATH ERROR" << endl;
+	}
+	else {
+		cout << result << endl;
 	}
 }
+
 void flog2() {
 	//Variables declaration
 	double x, one, proc, us;
@@ -475,17 +616,43 @@ void flog2() {
 	__asm {
 		//log2(x)
 		fld[one] //ST(1) = 1
-		fld[x] //ST(0) = x
-		fyl2x //ST(1) = (ST(1) * log2(ST(0))) -> ST(1) = 1 * log2(x)
-		fstp[proc] //proc = ST(1)
+			fld[x] //ST(0) = x
+			fyl2x //ST(1) = (ST(1) * log2(ST(0))) -> ST(1) = 1 * log2(x)
+			fstp[proc] //proc = ST(1)
 	}
 	cout << "****** RESULTADO (coprocesador): " << proc << endl;
 
-	//ASM code (our implementation)
-	__asm {
+	cout << "****** RESULTADO (Series): ";
+	if (x > 0) {
+		//ASM code (our implementation)
+		double lnx, ln2, two = 2;
+		__asm {
+			//lnx = ln(x)
+			sub esp, 8
+				movsd xmm0, [x]
+				movsd[esp], xmm0
+				call fln_auxiliary
+				add esp, 8
+				fstp[lnx]
 
+				//ln2 = ln(2)
+				sub esp, 8
+				movsd xmm0, [two]
+				movsd[esp], xmm0
+				call fln_auxiliary
+				add esp, 8
+				fstp[ln2]
+
+				//us = lnx/ln2 -> ln(x)/ln(2)
+				movsd xmm0, [lnx]
+				divsd xmm0, [ln2]
+				movsd[us], xmm0
+		}
+		cout << us << endl;
 	}
-	cout << "****** RESULTADO (Series): " << endl;
+	else {
+		cout << "MATH ERROR" << endl;
+	}
 }
 
 void flog10() {
@@ -502,36 +669,62 @@ void flog10() {
 	__asm {
 		//log2(10)
 		fld[one] //ST(1) = 1
-		fld[ten] //ST(0) = 10
-		fyl2x //ST(1) = (ST(1) * log2(ST(0))) -> ST(1) = 1 * log2(10)
-		fstp[var] //var = ST(1)
+			fld[ten] //ST(0) = 10
+			fyl2x //ST(1) = (ST(1) * log2(ST(0))) -> ST(1) = 1 * log2(10)
+			fstp[var] //var = ST(1)
 
-		//1/log2(10)
-		movsd xmm0, [one]
-		divsd xmm0, [var]
-		movsd[var], xmm0 //var = 1/log2(10)
+			//1/log2(10)
+			movsd xmm0, [one]
+			divsd xmm0, [var]
+			movsd[var], xmm0 //var = 1/log2(10)
 
-		//(1/log2(10)) * log2(x) -> log10(x)
-		fld[var] //ST(1) = var -> ST(1) = 1/log2(10)
-		fld[x] //ST(0) = x
-		fyl2x //ST(1) = (ST(1) * log2(ST(0))) -> ST(1) = (1/log2(10)) * log2(x) -> ST(1) = log10(x)
-		fstp[proc] //proc = ST(1)
+			//(1/log2(10)) * log2(x) -> log10(x)
+			fld[var] //ST(1) = var -> ST(1) = 1/log2(10)
+			fld[x] //ST(0) = x
+			fyl2x //ST(1) = (ST(1) * log2(ST(0))) -> ST(1) = (1/log2(10)) * log2(x) -> ST(1) = log10(x)
+			fstp[proc] //proc = ST(1)
 
 	}
 	cout << "****** RESULTADO (coprocesador): " << proc << endl;
 
-	//ASM code (our implementation)
-	__asm {
+	cout << "****** RESULTADO (Series): ";
+	if (x > 0) {
+		//ASM code (our implementation)
+		double lnx, ln10, ten = 10;
+		__asm {
+			//lnx = ln(x)
+			sub esp, 8
+				movsd xmm0, [x]
+				movsd[esp], xmm0
+				call fln_auxiliary
+				add esp, 8
+				fstp[lnx]
 
+				//ln10 = ln(10)
+				sub esp, 8
+				movsd xmm0, [ten]
+				movsd[esp], xmm0
+				call fln_auxiliary
+				add esp, 8
+				fstp[ln10]
+
+				//us = lnx/lnq0 -> ln(x)/ln(10)
+				movsd xmm0, [lnx]
+				divsd xmm0, [ln10]
+				movsd[us], xmm0
+		}
+		cout << us << endl;
 	}
-	cout << "****** RESULTADO (Series): " << endl;
+	else {
+		cout << "MATH ERROR" << endl;
+	}
 }
 
 void feuler() {
 	//Variables declaration
 	int iteration = 0, den;
-	double degrees, num, dden, result = 0,x;
-	
+	double num, dden, result = 0, x;
+
 	//double sin, dsin, cos, dcos;
 	//Variables initialization
 	cout << "****** INGRESA EL ARGUMENTO: ";
@@ -543,38 +736,38 @@ void feuler() {
 	taylor:
 		//(x)^n
 		push[iteration] //Push final argument (exponent) (n = tolerance)
-		sub esp, 8
-		movsd xmm0, [x]
-		movsd[esp], xmm0 //Push first argument (base)(x)
-		call fpot_auxiliary //Call pot
-		add esp, 0Ch
-		fstp[num] //num = x^n
+			sub esp, 8
+			movsd xmm0, [x]
+			movsd[esp], xmm0 //Push first argument (base)(x)
+			call fpot_auxiliary //Call pot
+			add esp, 0Ch
+			fstp[num] //num = x^n
 
-		//(n)!
-		push[iteration] //push iteration
-		call ffact_auxiliary //call factorial
-		add esp, 4
-		mov[den], eax //den = (n)!
-		cvtsi2sd xmm0, [den]
-		movsd[dden], xmm0
+			//(n)!
+			push[iteration] //push iteration
+			call ffact_auxiliary //call factorial
+			add esp, 4
+			mov[den], eax //den = (n)!
+			cvtsi2sd xmm0, [den]
+			movsd[dden], xmm0
 
-		//e^x = x^n / (n)!
-		movsd xmm0, [num] //x^n
-		divsd xmm0, [dden] //x^n/(n)!
-		addsd xmm0, [result] //result += x^n/(n)!
-		movsd[result], xmm0 
+			//e^x = x^n / (n)!
+			movsd xmm0, [num] //x^n
+			divsd xmm0, [dden] //x^n/(n)!
+			addsd xmm0, [result] //result += x^n/(n)!
+			movsd[result], xmm0
 
-		//Repeat until iteration < limit 
-		inc[iteration]
-		cmp[iteration], 9 //Change this value for chage the number of repetitions
-		jne taylor
+			//Repeat until iteration < limit 
+			inc[iteration]
+			cmp[iteration], 10 //Change this value for chage the number of repetitions
+			jne taylor
 	}
 
 	cout << "****** RESULTADO (Series): " << result << endl;
 }
 
 void printMenu() {
-	cout<< "********************************************************" << endl 
+	cout << "********************************************************" << endl
 		<< "****** CALCULATHOR ******" << endl
 		<< "****** OPERACIONES BASICAS" << endl
 		<< "****** 1. SUMA" << endl
@@ -585,15 +778,17 @@ void printMenu() {
 		<< "****** 5. SENO" << endl
 		<< "****** 6. COSENO" << endl
 		<< "****** 7. TANGENTE" << endl
-		<< "****** 8. - RAIZ CUADRADA" << endl
+		<< "****** 8. RAIZ CUADRADA" << endl
 		<< "****** 9. POTENCIA" << endl
 		<< "****** 10. LOGARITMO EN BASE 2 'log2(x)'" << endl
-		<< "****** 11. - LOGARITMO EN BASE 10 'log10(x)'" << endl
+		<< "****** 11. LOGARITMO EN BASE 10 'log10(x)'" << endl
 		<< "****** 12. EULER" << endl
 		<< "****** 13. FACTORIAL" << endl
+		<< "****** 14. LOGARITMO NATURAL" << endl
+		<< "****** 15. ABS" << endl
 		<< "****** 0 SALIR" << endl
 		<< "****** INGRESA ELECCION: ";
-		
+
 }
 
 int main() {
@@ -645,7 +840,10 @@ int main() {
 			ffact();
 			break;
 		case 14: //ln
-			fln(3);
+			fln();
+			break;
+		case 15: //abs
+			abs();
 			break;
 		default:
 			cout << "****** OPCION INVALIDA ******" << endl;
